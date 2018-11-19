e2659b1e372c9f2a64fb601cc8d5bea4c6529dd4
lalit <chauhan.lalit95@gmail.com>
2018-11-19 16:22:29 +0530

    footer changes

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,41 +1,33 @@
 
 <footer class="footer">
 	<div class="container">
-       <div class="top">
-           <ul>
-               <li><a href="#">Webmail</a></li>
-               <li>|</li>
-               <li><a href="#">Terms & Conditions</a></li>
-               <li>|</li>
-               <li><a href="#">Privacy Policy </a></li>
-               <li>|</li>
-               <li><a href="#">Site Map</a></li>
-               <li>|</li>
-               <li><a href="#">Contact Us </a></li>
-               <li>|</li>
-               <li><a href="#">Feedback</a></li>
-               <li>|</li>
-               <li><a href="#">Tenders</a></li>
-               <li>|</li>
-               <li><a href="#">Careers</a></li>
-           </ul>
+   <div class="top">
+     <ul>
+       <li><a href="#">Site Map</a></li>
+       <li>|</li>
+       <li><a href="<?php echo base_url('contact'); ?>">Contact Us </a></li>
+       <li>|</li>
+       <li><a href="<?php echo base_url('contact/feedback'); ?>">Feedback</a></li>
+       <li>|</li>
+       <li><a href="<?php echo base_url('contact/career'); ?>">Careers</a></li>
+     </ul>
 
-           <p class="center">Visitor Counter: 24,25445</p>
+     <!-- <p class="center">Visitor Counter: 24,25445</p> -->
 
-       </div><!--close top-->
+   </div><!--close top-->
 
-   </div>
+ </div>
 
-   <div class="bottom">
-    <p>© Owned by  NEON HOSPITAL, Thane.</p>
+ <div class="bottom">
+  <p>© Owned by  NEON HOSPITAL, Thane.</p>
 
 </div>
 </div>
 
 </footer><!--close footer-->
 <script src="<?php echo base_url(); ?>assets/js/moment.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/jquery-2.2.4.min.js"></script>
-  <script src="<?php echo base_url(); ?>assets/js/bootstrap-datetimepicker.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jquery-2.2.4.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-datetimepicker.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/lightbox-plus-jquery.min.js"></script>
